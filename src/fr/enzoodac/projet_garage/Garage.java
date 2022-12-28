9839e557d66d1afadc6dfcc5e7355f908917a092
root <enzodac@live.fr>
2022-12-28 12:24:50 +0100

    fonction ajouter personnel garage

--- src/fr/enzoodac/projet_garage/Garage.java
+++ src/fr/enzoodac/projet_garage/Garage.java
@@ -6,10 +6,25 @@ import fr.enzoodac.projet_garage.stock.Consommables;
 
 public class Garage {
 	private String nom;
-	private Personnel[] personnel;
 	private int nbPersonnel = 0;
+	private int nbClient = 0;
+	private Personnel[] personnel;
 	private Client[] client;
 	private Consommables[] consommables;
-	
-	
+
+	public Garage(String nom, int nbPersonnelMax, int nbClientMax) {
+		super();
+		this.nom = nom;
+		client = new Client[nbClientMax];
+		personnel = new Personnel[nbPersonnelMax];
+	}
+
+	public void ajouterPersonnel(Personnel personnel) {
+		if (nbPersonnel < this.personnel.length) {
+			this.personnel[nbPersonnel] = personnel;
+			nbPersonnel++;
+		}
+
+	}
+
 }
