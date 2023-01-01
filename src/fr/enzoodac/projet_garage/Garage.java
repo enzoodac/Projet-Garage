4a0a3c264f0e506a1da56d3d2a8c1b61663621a6
root <enzodac@live.fr>
2023-01-01 18:06:28 +0100

    list hash

--- src/fr/enzoodac/projet_garage/Garage.java
+++ src/fr/enzoodac/projet_garage/Garage.java
@@ -9,85 +9,57 @@ import fr.enzoodac.projet_garage.stock.Consommables;
 
 public class Garage {
 	private String nom;
-	private int nbPersonnel = 0;
-	private int nbClient = 0;
-	private int nbPersonnelMax;
-	private int nbClientMax;
 	private int budget;
-	private Personnel[] personnel;
 	private Client[] client;
-	private Set<Client> clients = new HashSet<>(); 
-
+	private Set<Client> clients = new HashSet<>();
+	private Set<Personnel> personnel = new HashSet<>();
 
 	private Consommables[] consommables;
 
-	public Garage(String nom, int nbPersonnelMax, int nbClientMax, int budget) {
+	public Garage(String nom, int budget) {
 		super();
 		this.nom = nom;
-		this.nbClient = nbClient;
-		this.nbClientMax = nbClientMax;
-		client = new Client[nbClientMax];
-		personnel = new Personnel[nbPersonnelMax];
+		
 	}
 
 	public void ajouterPersonnel(Personnel personnel) {
-		if (nbPersonnel < this.personnel.length) {
-			this.personnel[nbPersonnel] = personnel;
-			nbPersonnel++;
-		}
+		this.personnel.add(personnel);
+	}
+	
+	public void supprimerPersonnel(Personnel personnel) {
+		this.personnel.add(personnel);
 	}
 
 	public void ajouterClient(Client client) {
-		if (nbClient < this.client.length) {
-			this.client[nbClient] = client;
-			nbClient++;
-		}
+		clients.add(client);
 
 	}
 
-	public void supprimerClient(int clientCherche) {
-		Client[] nouveau = new Client[nbClientMax];
-		int i = 0;
-		while (i < nbClient) {
-			if (i != clientCherche) {
-				nouveau[i] = client[i];
-				i++;
-			}
+	public void supprimerClient(Client client) {
+		clients.remove(client);
 
-		}
-
-		nbClient--;
-		client = nouveau;
-
-	}
-
-	public int trouverIndiceClient(String prenom, String nom) {
-		int i = 0;
-
-		while (i < nbClient && this.client[i].getNom() != nom && this.client[i].getPrenom() != prenom) {
-			i++;
-		}
-		return i;
 	}
 
 	public void citerClients() {
-		for (int i = 0; i < nbClient; i++) {
-			System.out.println(client[i].getPrenom());
-		}
+
+		for (Client s : clients)
+			System.out.println(s.getNom());
+	}
+	public void citerPersonnel() {
+
+		for (Personnel e : personnel)
+			System.out.println(e);
 	}
 
 	public static void main(String[] args) {
-		Garage test = new Garage("test", 4, 3, 10000);
+		Garage test = new Garage("test", 30000);
 		Client enzo = new Client("da cunha", "enzo", "0652592615", 3000, "iolijg@rfg.fr");
 		Client client = new Client("client", "test", "0652326524", 1500, "enzodac@live.fr");
 		test.ajouterClient(client);
 		test.ajouterClient(enzo);
-
+		
 		test.citerClients();
-		System.out.println(test.nbClient);
-		test.supprimerClient(1);
-		System.out.println(test.nbClient);
-		test.citerClients();
+		
 
 	}
 
